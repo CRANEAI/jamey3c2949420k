4746cf3e1d90ec20d2bcac7c134b29a27f4b3419
Elly <elly@mg.crane.ai>
2018-11-03 12:37:51 -0400

    added client/src/reducers/index refactoring

---
+++ client/src/reducers/index.jsx
@@ -0,0 +1,16 @@
+
+        import { combineReducers }                      from 'redux';
+        import { reducer as Form }                      from 'redux-form';
+
+        import round_1_/_25 from './reducer_round_1_/_25' 
+import geography from './reducer_geography'
+
+        const rootReducer = combineReducers({
+            round_1_/_25 : round_1_/_25 
+,geography : geography 
+
+        })
+
+        export default rootReducer;
+
+        
