b142c62283ae1f17e650d83ca1ffb857aa935b37
Elly <elly@mg.crane.ai>
2018-11-03 12:39:45 -0400

    added server/models/CityHall refactoring

---
+++ server/models/CityHall.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+
+var Schema = new mongoose.Schema({
+  id: String,
+  createdAt: String,
+  updatedAt: String,
+  userRef: String,
+  additionalType: String,
+  alternateName: String,
+  description: String,
+  image: String,
+  name: String,
+  sameAs: String,
+  url: String,
+  address: String,
+  aggregateRating: String,
+  containedIn: String,
+  event: String,
+  events: String,
+  faxNumber: String,
+  geo: String,
+  globalLocationNumber: String,
+  interactionCount: String,
+  isicV4: String,
+  logo: String,
+  map: String,
+  maps: String,
+  openingHoursSpecification: String,
+  photo: String,
+  photos: String,
+  review: String,
+  reviews: String,
+  telephone: String,
+  openingHours: String
+})
+
+module.exports = mongoose.model("CityHall", Schema)
