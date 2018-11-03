216c3f9b2623cb53c1a4457df55b56644ad01cb0
Elly <elly@mg.crane.ai>
2018-11-03 14:12:19 -0400

    updated tests/geography refactoring

--- tests/geography.test.js
+++ tests/geography.test.js
@@ -7,10 +7,10 @@
  * jamey 3
 */
 
-/* the round 1 / 25 view has a add icon located at the top center of the view which on click will trigger the add() action */
-it("the round 1 / 25 view has a add icon located at the top center of the view which on click will trigger the add() action", () => {
+/* view has actions which trigger on events, view should should error messages and display activity indicator when processing */
+it("view has actions which trigger on events, view should should error messages and display activity indicator when processing", () => {
   /* ToDo developer needs to complete unit test 
-               the round 1 / 25 view has a add icon located at the top center of the view which on click will trigger the add() action
+               view has actions which trigger on events, view should should error messages and display activity indicator when processing
                uncomment below and update the code to test
                 //instance.action(function(error) {
                 //  if (error) throw error;
